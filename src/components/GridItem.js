7e9712b468fc6bfd5846ef2b5812c98c0c717fdb
fustosgergo <32802459+fustosgergo@users.noreply.github.com>
2024-03-05 14:22:24 +0100

    Working minimal with redux

--- src/components/GridItem.js
+++ src/components/GridItem.js
@@ -1,17 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import GraphBlock from "./GraphBlock";
-import Title from "./Title";
-
 const GridItem = ({
   title,
-  data,
-  type,
   className,
   style,
-  dispatch,
-  root,
   children,
   ...rest
 }) => {
@@ -20,10 +13,10 @@ const GridItem = ({
   return (
     <div className={`grid-item ${className}`} style={style} {...rest}>
       <div className="grid-item__title">
-        <Title title={title} type={type} root={root} />
+        <h3>{title}</h3>
       </div>
       <div className="grid-item__graph">
-        <GraphBlock type={type} data={data} width={width} height={height} />
+        <></>
       </div>
       {children}
     </div>
@@ -32,9 +25,6 @@ const GridItem = ({
 
 GridItem.propTypes = {
   title: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
-  data: PropTypes.array.isRequired,
-  root: PropTypes.string.isRequired,
   children: PropTypes.array
 };
 
--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,30 +1,20 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { Route, Switch } from "react-router";
 
 import GridLayout from "../components/GridLayout";
-import LayoutEditor from "../components/LayoutEditor";
 
-const Dashboard = ({ data, id, layouts }) => {
+const Dashboard = ({ data, layouts }) => {
   return (
     <div className="dashboard">
       <GridLayout data={data} layouts={layouts} />
-      <Route exact path="/:id/layout" component={LayoutEditor} />
     </div>
   );
 };
 
-Dashboard.propTypes = {
-  data: PropTypes.arrayOf(PropTypes.string).isRequired,
-  layouts: PropTypes.object.isRequired
-};
-
-const mapStateToProps = (state, { match: { params: { id } } }) => {
+const mapStateToProps = (state) => {
   return {
     data: Object.keys(state.data),
-    layouts: { ...state.layouts },
-    id
+    layouts: { ...state.layouts }
   };
 };
 
